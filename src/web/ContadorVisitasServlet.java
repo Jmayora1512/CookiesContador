1a3c0061dac97a54e841f6614dc777e91ddff3f4
Jmayora1512 <jorgemayora88@gmail.com>
2023-02-02 12:52:12 +0100

    Se agrega el proyecto

---
+++ src/web/ContadorVisitasServlet.java
@@ -0,0 +1,71 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContadorVisitasServlet
+ */
+@WebServlet("/ContadorVisitasServlet")
+public class ContadorVisitasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ContadorVisitasServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+    	
+        //Declaramos la variable contador
+        int contador = 0;
+        
+        //Revisamos si existe la cookie contadorVisitas
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie c: cookies){
+                if(c.getName().equals("contadorVisitas")){
+                    contador = Integer.parseInt(c.getValue());
+                }
+            }
+        }
+        
+        //incrementamos el contador el uno
+        contador++;
+        //Agregamos la respuesta al navegador
+        Cookie c = new Cookie("contadorVisitas",Integer.toString(contador));
+        //la cookie se almacenara en el cliente por 1 hora (3600 seg)
+        c.setMaxAge(3600);
+        response.addCookie(c);
+        
+        //Mandamos el mensaje al navegador
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("Contador de visitas de cada cliente:" + contador);    	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
